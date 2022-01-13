bbbd5161d95afa099faa3d6bfdb7c73ca0eea7ac
Library Laptop <laptop@NUL-LENDLAP23.library.northwestern.edu>
2022-01-12 21:18:36 -0600

    Fixed text in MUI list to display name from json.

--- src/App.js
+++ src/App.js
@@ -11,16 +11,12 @@ const App = () => {
   const [groceryList, loading, error] = useData("/");
 
   if (error) return <h1>{error}</h1>;
-  if (loading) return <h1>Loading the schedule...</h1>;
+  if (loading) return <h1>Loading the grocery list...</h1>;
 
   return (
     <div className="App">
       <GroceryList items = { groceryList.items } />
-      <ul>
-        {groceryList.items.map((value) => (
-          <li>{value.name}</li>
-        ))}
-      </ul>
+      
     </div>
   );
 };
