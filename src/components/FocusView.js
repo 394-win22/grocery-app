9bd9f2a51839c8ec5e4be00ae461da06cd2bbe0b
alejandrodavidmalavet <61026152+alejandrodavidmalavet@users.noreply.github.com>
2022-01-30 17:35:20 -0600

    Added notes function and subtitles

--- src/components/FocusView.js
+++ src/components/FocusView.js
@@ -4,32 +4,75 @@ import { setData } from "../utilities/firebase";
 import QuantityByPerson from "./focusView/QuantityByPerson";
 import AddSubtractButtons from "./focusView/AddSubtractButtons";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Button, TextField } from "@mui/material";
 
 const FocusView = ({ item, user, usersInfo }) => {
+  const [editMode, setEditMode] = React.useState(false);
+  const EditNote = () => {
+    const [newNote, setNewNote] = React.useState(item.notes);
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setData(`/items/${item.name}/notes/`, newNote);
+      setNewNote("");
+      setEditMode(false);
+    };
+    return (
+      <form className="edit-note" onSubmit={handleSubmit}>
+        <TextField
+          style={{ display: "block", margin: "0px", padding: "0px" }}
+          id="note-edit"
+          variant="outlined"
+          onInput={(e) => setNewNote(e.target.value)}
+          value={newNote}
+        />
+        <Button
+          type="submit"
+          variant="text"
+          style={{ display: "block", margin: "0px", padding: "0px" }}
+        >
+          Add Note
+        </Button>
+      </form>
+    );
+  };
   return (
     <AccordionDetails>
       {/* //notes */}
-      <ListItemText>{item.notes}</ListItemText>
+      {editMode ? (
+        <EditNote />
+      ) : (
+        <>
+          <Button
+            variant="text"
+            style={{ display: "block", margin: "0px", padding: "0px" }}
+            onClick={() => {
+              setEditMode(true);
+            }}
+          >
+            Edit Note
+          </Button>
+        </>
+      )}
+
       {/* //add and subtract quantity //delete button */}
       {/* //who want what */}
       <QuantityByPerson quantityDict={item.quantity} usersInfo={usersInfo} />
       <IconButton
         aria-label="delete"
         size="small"
-        onClick={!item.quantity[user['uid']]
-          ? null : () => {
-          setData(`/items/${item.name}`, null);
-          // delete item;
-        }}
+        onClick={
+          !item.quantity[user["uid"]]
+            ? null
+            : () => {
+                setData(`/items/${item.name}`, null);
+                // delete item;
+              }
+        }
       >
         <DeleteIcon fontSize="small" />
       </IconButton>
     </AccordionDetails>
   );
-};
-
-const editNote = () => {
-  // TODO: edit note
 };
 
 export default FocusView;
