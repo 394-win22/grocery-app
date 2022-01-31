ec5cc49ad36dcf101a3b80f3112ec67d59b5b68f
kevinsu2024 <su.kevin8@gmail.com>
2022-01-30 23:17:22 -0600

    styled checkout button

--- src/components/CheckoutButton.js
+++ src/components/CheckoutButton.js
@@ -5,7 +5,7 @@ import { Button} from "@mui/material";
 
 export default function CheckoutButton({items}) {
     const handleClick = () => {
-        
+        Object.values()
     }
   return (
       <Button variant="contained"
